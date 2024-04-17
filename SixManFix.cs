6a1be274590b60fc3acff9ad8f381b00399b1c90
niefortunny <1niefortunny@gmail.com>
2024-04-17 17:19:04 +0200

    complete overhaul

--- SixManFix.cs
+++ SixManFix.cs
@@ -2,40 +2,41 @@ using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes.Registration;
 using CounterStrikeSharp.API.Modules.Utils;
 
-namespace SixManFix
+namespace SixManFix;
+
+public class SixManFix : BasePlugin
 {
-    public class SixManFix : BasePlugin
+    public override string ModuleName => "SixManFix";
+    public override string ModuleVersion => "1.0.3";
+    public override string ModuleAuthor => "unfortunate";
+
+    #region Events
+    [GameEventHandler(HookMode.Pre)]
+    public HookResult OnPlayerDisconnect(EventPlayerDisconnect @event, GameEventInfo info)
     {
-        public override string ModuleName => "SixManFix";
-        public override string ModuleVersion => "1.0.2";
-        public override string ModuleAuthor => "unfortunate";
-
-        #region Events
-        [GameEventHandler(HookMode.Pre)]
-        public HookResult OnPlayerConnect(EventPlayerConnect @event, GameEventInfo info)
-        {
-            var player = @event.Userid;
-            if (!IsPlayerValid(player)) return HookResult.Continue;
-            if (player.TeamNum == 0) return HookResult.Continue;
-
-            player.ChangeTeam(CsTeam.None);
-            player.ExecuteClientCommand("teammenu");
-
+        var player = @event.Userid;
+        if (!IsPlayerValid(player))
             return HookResult.Continue;
-        }
-        #endregion
+        if (player.TeamNum == 0)
+            return HookResult.Continue;
 
-        #region Functions
-        public static bool IsPlayerValid(CCSPlayerController player)
-        {
-            return player != null
-                && player.IsValid
-                && !player.IsBot
-                && player.Pawn != null
-                && player.Pawn.IsValid
-                && player.Connected == PlayerConnectedState.PlayerConnected
-                && !player.IsHLTV;
-        }
-        #endregion
+        player.ChangeTeam(CsTeam.None);
+        Console.WriteLine($"[SixManFix] {player.PlayerName} switched to CsTeam.None");
+
+        return HookResult.Continue;
     }
+    #endregion
+
+    #region Functions
+    public static bool IsPlayerValid(CCSPlayerController player)
+    {
+        return player != null
+            && player.IsValid
+            && !player.IsBot
+            && player.Pawn != null
+            && player.Pawn.IsValid
+            && player.Connected == PlayerConnectedState.PlayerConnected
+            && !player.IsHLTV;
+    }
+    #endregion
 }
